63b05fd98d0b5b691384a5d5f99d81c9f86013f8
Ulric Qin <ulric.qin@gmail.com>
2025-03-29 10:54:18 +0800

    code format

--- extension.js
+++ extension.js
@@ -4,7 +4,7 @@ const vscode = require('vscode');
 
 function formatCurrentTime() {
 	const currentTime = new Date();
-  
+
 	const year = currentTime.getFullYear();
 	const month = String(currentTime.getMonth() + 1).padStart(2, '0');
 	const day = String(currentTime.getDate()).padStart(2, '0');
@@ -17,9 +17,9 @@ function formatCurrentTime() {
 	const timezoneMinutes = String(Math.abs(timezoneOffset) % 60).padStart(2, '0');
 	const timezoneSign = timezoneOffset >= 0 ? '+' : '-';
 	const timezone = `${timezoneSign}${timezoneHours}:${timezoneMinutes}`;
-  
+
 	return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${timezone}`;
-  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -32,7 +32,7 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "insertisodate" is now active!');
-	
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -46,19 +46,19 @@ function activate(context) {
 		let dateString = formatCurrentTime();
 
 		editor.edit(function (text) {
-            // get current selection
-            let selection = editor.selection;
+			// get current selection
+			let selection = editor.selection;
 
-            if (!selection.isEmpty) {
-                // Replace selected text with dateString
-                text.replace(selection, dateString);
-            } else {
-                // Insert dateString at current cursor position
-                let startLine = selection.start.line;
-                let startCharacter = selection.start.character;
-                text.insert(new vscode.Position(startLine, startCharacter), dateString);
-            }
-        });
+			if (!selection.isEmpty) {
+				// Replace selected text with dateString
+				text.replace(selection, dateString);
+			} else {
+				// Insert dateString at current cursor position
+				let startLine = selection.start.line;
+				let startCharacter = selection.start.character;
+				text.insert(new vscode.Position(startLine, startCharacter), dateString);
+			}
+		});
 
 	});
 
@@ -66,7 +66,7 @@ function activate(context) {
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
